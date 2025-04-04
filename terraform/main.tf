90968079455b48b99b5ca5ab8e8e77c2b1b6540c
hacwa <20915626+hacwa@users.noreply.github.com>
2025-04-04 17:58:03 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -143,7 +143,7 @@ resource "cloudflare_dns_record" "jenkins_hacwa_co_uk" {
   zone_id = "19cf385e2fe2d7760da58d3b795efe35"
   name    = "jenkins.hacwa.co.uk."
   type    = "CNAME"
-  content = "vip1.hacwa.co.uk."
+  content = "vip2.hacwa.co.uk."
   ttl     = 60
   proxied = false
 }
