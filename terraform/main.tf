402d5a34b23ecddfd94adc7729d5c3a03090bef5
hacwa <20915626+hacwa@users.noreply.github.com>
2025-04-05 07:51:45 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,7 @@ resource "cloudflare_dns_record" "argocd_hacwa_co_uk" {
   zone_id = "19cf385e2fe2d7760da58d3b795efe35"
   name    = "argocd.hacwa.co.uk."
   type    = "CNAME"
-  content = "vip1.hacwa.co.uk."
+  content = "vip2.hacwa.co.uk."
   ttl     = 60
   proxied = false
 }
