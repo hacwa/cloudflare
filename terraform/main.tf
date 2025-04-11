16f2b4ae45e8bf0a41899dcc71e9e167a39beacd
Stuart Kennedy <stuart.kennedy@capgemini.com>
2025-04-11 11:05:23 +0100

    sdf

--- terraform/main.tf
+++ terraform/main.tf
@@ -280,7 +280,7 @@ resource "cloudflare_dns_record" "synapse_hacwa_co_uk" {
   zone_id = "19cf385e2fe2d7760da58d3b795efe35"
   name    = "synapse.hacwa.co.uk."
   type    = "CNAME"
-  content = "vip1.hacwa.co.uk."
+  content = "vip2.hacwa.co.uk."
   ttl     = 60
   proxied = false
 }
